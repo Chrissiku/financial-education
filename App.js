ee9f43c05bdcc166595f370a3ccf6e2f01097900
Chris Siku <chrissiku5@gmail.com>
2023-11-10 00:22:00 +0200

    Refactor App

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
-import LoginScreen from "./App/Education/Pages/LoginScreen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <StatusBar />
-      <LoginScreen />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- App/common/Theme.js
+++
@@ -1,4 +0,0 @@
-export default {
-  primary: "#0c7de4",
-  white: "#fff",
-};
