089c159bf1dcfdb1d52b5943d30ae2db58139df9
Chris Siku <chrissiku5@gmail.com>
2023-11-10 13:38:57 +0200

    Install Strapi and configure cloudinary

---
+++ backend/config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: "ddouo0mky",
+        api_key: "592821557915977",
+        api_secret: "s6QuGVXF-johgpertlgIbW4c3fY",
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
